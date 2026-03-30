2aef95ea5629f0b96b1603492c36d7bfb9f97366
KOshima85 <secret_track_is@yahoo.co.jp>
2026-03-30 23:21:00 +0900

    未使用のSetModelNameを削除

--- Source/OllamaConnector.h
+++ Source/OllamaConnector.h
@@ -7,14 +7,11 @@ public:
 	virtual void Initialize() override;
 	virtual std::string Call(const std::string& prompt) override;
 
-	void SetModelName(char modelName) { m_sModelName[0] = modelName; m_sModelName[1] = '\0'; }	
 	OllamaConnector(const Config* cfg);
 
 private:
 	char m_sModelName[256];
 	void SetModelName(const char* modelName);
-	// 文字列のサニタイズ関数
-	//char sanitizeModelName(const char* modelName);
 	std::string SanitizeModelName(std::string_view modelName);
 
 };
